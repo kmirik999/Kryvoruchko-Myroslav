22a1dc530ace724ab68c1962be0d0c89a669b9a0
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2021-12-01 09:38:48 +0200

    fix

--- 0-buggy_1.js
+++ 0-buggy_1.js
@@ -1,5 +1,4 @@
-const sum = [189,1000].reduce(add, 0); // with initial value to avoid when the array is empty
-
+const sum = [189,1000].reduce(add, 0); 
 function add(accumulator, a) {
   return accumulator + a;
 }
