96c2834cf7bfea60162bbf2b44d83a30a00f06cf
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2021-12-01 22:22:20 +0200

    improved

---
+++ 0-buggy_2improved.js
@@ -0,0 +1,8 @@
+const sum = (...args) => {
+    let totalSum = 0;
+    for (const numbers of args) {
+        totalSum += numbers;
+      }
+    return totalSum
+  };
+  console.log(sum(299, 1, 10, 90))
