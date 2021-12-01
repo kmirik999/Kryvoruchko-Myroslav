2a9571a62de3d2b416f583803cc9fbe3f9d1105f
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2021-12-01 22:15:40 +0200

    fix

--- 0-buggy.py
+++ 0-buggy.py
@@ -1,8 +1,6 @@
-def sum(*args):
-    total_sum = 0
-    for number in args:
-        total_sum += number
-    return total_sum
-result = sum(1,3,4,5,8,9,16)
-print({result})
+first_num = 2
+second_num = 3
+sum = first_num + second_num
+print(sum)
+
 
